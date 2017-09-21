946b66afbd51e55403729a6d534dd3abb3a453b2
344789508 <344789508@C-3ZWJYR1.tdsb.on.ca>
2017-09-21 10:27:03 -0400

    Hello World and unit one

---
+++ Unit 1/src/cheung/HelloWorld.java
@@ -0,0 +1,16 @@
+package cheung;
+/**
+ * HelloWorld.java 
+ * This is a program that prints hello world
+ * September 20, 2017
+ * @author Naomi Cheung
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.print("Hello World");
+
+	}
+
+}
